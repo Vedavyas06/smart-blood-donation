1f1e548856009cdfd0e24bb1c9a598057a936115
Vedavyas06 <vedavyasofficial06@gmail.com>
2025-09-14 01:36:14 +0530

    implemented request model and email service

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,25 +14,27 @@ app.use(cookieParser())
 
 import userRoute from "./routes/donor.route.js"
 import hospitalRoute from "./routes/hospital.route.js" 
+import requestRoutes from "./routes/request.route.js";
 app.use("/donor", userRoute)
 app.use("/hospital", hospitalRoute)
+app.use("/requests", requestRoutes);
 
-app.use((err, req, res, next) => {
-    console.error(err); // for debugging
+// app.use((err, req, res, next) => {
+//     console.error(err); // for debugging
 
-    if (err instanceof ApiError) {
-            return res.status(err.statusCode).json({
-                statusCode: err.statusCode,
-                success: err.success,
-                message: err.message,  
-                data: err.data,
-                errors: err.errors
-        });
-    }
+//     if (err instanceof ApiError) {
+//             return res.status(err.statusCode).json({
+//                 statusCode: err.statusCode,
+//                 success: err.success,
+//                 message: err.message,  
+//                 data: err.data,
+//                 errors: err.errors
+//         });
+//     }
 
-    return res.status(500).json({
-        success: false,
-        message: "Internal Server Error",
-    });
-});
+//     return res.status(500).json({
+//         success: false,
+//         message: "Internal Server Error",
+//     });
+// });
 export default app
