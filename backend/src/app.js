7f49a792551ff54f9c3e2b44e70431a7bee55e4e
Vedavyas06 <vedavyasofficial06@gmail.com>
2025-09-17 18:33:46 +0530

    changed bugs

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,7 @@
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
-import { ApiError } from "./utils/ApiError.js"
+import { ApiError } from "./utils/apiError.js"
 const app = express()
 app.use(cors({
     origin: "http://localhost:5173",
