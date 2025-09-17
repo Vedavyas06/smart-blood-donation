9b69e9d4469c59c00ecdc8aba6b4bec2d7bb5837
Vedavyas06 <vedavyasofficial06@gmail.com>
2025-09-17 18:38:05 +0530

    changed folder names

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,7 @@
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
-import { ApiError } from "./utils/apiError.js"
+import { ApiError } from "./utils/ApiError.js"
 const app = express()
 app.use(cors({
     origin: "http://localhost:5173",
